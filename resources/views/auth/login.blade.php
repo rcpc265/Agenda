a36f3f4eca2887385f1ce40b0fedca86acc7e92d
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2023-06-15 09:29:50 -0500

    Autocompletar contraseña en ambiente local

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,40 +3,50 @@
 @section('title', 'Iniciar sesión')
 
 @section('content')
-<div class="container mt--8 pb-5">
+  <div class="container mt--8 pb-5">
     <div class="row justify-content-center">
       <div class="col-lg-5 col-md-7">
         <div class="card bg-secondary shadow border-primary">
 
           <div class="card-body px-lg-5 py-lg-5">
-                @if ($errors->any())
-                    <div class="text-center text-muted mb-2">
-                        <small>Se encontro el siguiente error</small>
-                    </div>
-                    <div class="alert alert-danger mb-4" role="alert">
-                        {{ $errors->first() }}
-                    </div>
-                @else
-                <div class="header-body text-center mb-2">
-                    <div class="row justify-content-center">
-                        <div class="col-lg- col-md-10">
-                            <h1 class="text-dark">@yield('title', 'Bienvenidos')</h1>
-                        </div>
-                    </div>
+            @if ($errors->any())
+              <div class="text-center text-muted mb-2">
+                <small>Se encontro el siguiente error</small>
+              </div>
+              <div class="alert alert-danger mb-4"
+                role="alert">
+                {{ $errors->first() }}
+              </div>
+            @else
+              <div class="header-body text-center mb-2">
+                <div class="row justify-content-center">
+                  <div class="col-lg- col-md-10">
+                    <h1 class="text-dark">@yield('title', 'Bienvenidos')</h1>
+                  </div>
                 </div>
-                    <div class="text-center text-muted mb-4">
-                        <small>Ingresa tus credenciales para ingresar al sistema</small>
-                    </div>
-                @endif
+              </div>
+              <div class="text-center text-muted mb-4">
+                <small>Ingresa tus credenciales para ingresar al sistema</small>
+              </div>
+            @endif
 
-            <form role="form" method="POST" action="{{ route('login') }}">
-                @csrf
+            <form role="form"
+              method="POST"
+              action="{{ route('login') }}">
+              @csrf
               <div class="form-group mb-3">
                 <div class="input-group input-group-alternative">
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                   </div>
-                  <input class="form-control" placeholder="Correo Electrónico" type="email" name="email" value="{{ old('email', 'soporte@soporte.com') }}" required autocomplete="email" autofocus>
+                  <input class="form-control"
+                    name="email"
+                    type="email"
+                    value="{{ old('email', 'soporte@soporte.com') }}"
+                    placeholder="Correo Electrónico"
+                    required
+                    autocomplete="email"
+                    autofocus>
                 </div>
               </div>
               <div class="form-group">
@@ -44,32 +54,46 @@
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
                   </div>
-                  <input class="form-control" placeholder="Contraseña" type="password" name="password" required autocomplete="current-password" value="soporte1">
+                  <input class="form-control"
+                    name="password"
+                    type="password"
+                    placeholder="Contraseña"
+                    required
+                    {{-- @dd(config('app.env')) --}}
+                    {{ config('app.env') === 'local' ? "value='soporte1'" : '' }}>
                 </div>
               </div>
               <div class="custom-control custom-control-alternative custom-checkbox">
-                <input name="remember" class="custom-control-input" id="remember" type="checkbox" {{ old('remember', 'checked') ? 'checked' : '' }}>
-                <label class="custom-control-label" for="remember">
+                <input class="custom-control-input"
+                  id="remember"
+                  name="remember"
+                  type="checkbox"
+                  {{ old('remember', 'checked') ? 'checked' : '' }}>
+                <label class="custom-control-label"
+                  for="remember">
                   <span class="text-muted">Recordar Sesión</span>
                 </label>
               </div>
               <div class="text-center">
-                <button type="submit" class="btn btn-primary my-4">Empezar</button>
+                <button class="btn btn-primary my-4"
+                  type="submit">Empezar</button>
               </div>
             </form>
           </div>
         </div>
         <div class="row mt-3">
           <div class="col-6">
-            <a href="{{ route('password.request') }}" class="text-dark"><small>¿Olvidaste tu contraseña?</small></a>
+            <a class="text-dark"
+              href="{{ route('password.request') }}"><small>¿Olvidaste tu contraseña?</small></a>
           </div>
-          <div class="col-6 text-right">
-            <a href="{{ route('register') }}" class="text-dark"><small>Crear nueva cuenta </small></a>
-          </div>
+          {{-- <div class="col-6 text-right">
+            <a class="text-dark"
+              href="{{ route('register') }}"><small>Crear nueva cuenta </small></a>
+          </div> --}}
         </div>
       </div>
     </div>
-</div>
+  </div>
 
 
 @endsection
